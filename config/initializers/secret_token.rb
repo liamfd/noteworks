1f7677a900ec1df6a49d3da4a5cb2d67ffbbeaca
liamfd <liamfd@gmail.com>
2013-11-07 20:22:40 -0500

    Just made the new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Noteweb::Application.config.secret_key_base = '431c4b57e5a901ee400565c4f0e976b1f8a221f03cd8bbf403f3c1dbf8277cdbe6c89a1e5a4da58322768de4dfaa5d82e3a36fd34df2b5aebcda166a6aeb89ca'
