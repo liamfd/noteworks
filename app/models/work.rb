8af999fc9c540a03dca5d293ff9c32eae19e5122
liamfd <liamfd@gmail.com>
2013-11-23 13:08:43 -0500

    Broke down string input into what it needs to be to make new nodes.

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,23 +5,33 @@ class Work < ActiveRecord::Base
 
 	def parseText
 		markup.each_line do |line|
+			puts "\n--------\n"
 			#parser rules: any amount of whitespace followed immediately by < means new node. Otherwise, new note.
 			#<TYPE.CATEGORY>TITLE
 			puts line
 			#if the occurence of <*> is before the first occurence of " then it's a new
-			@angleBracketLocation = line.index(/<.*>/)
-			@firstQuoteMark = line.index("\"")
-			puts @angleBracketLocation
-			puts @firstQuoteMark
-			if @angleBracketLocation < @firstQuoteMark #this means it's time to make a new one! must check if it's a child
+			#@angleBracketLocation = line.index(/[ ,\t]*<.*>/)
+			@firstChar = line.match(/[ ,\t]*(.)/).captures.first
+			if @firstChar == '<'
 				puts "Nailed it. Making a new one."
+				@withinBrackets = line.match(/<.*>/).to_s
+				puts @withinBrackets
+
+				#get type, convert it to a constant
+				puts @type
+				@type = @withinBrackets.match(/<(.*)\./).captures.first
+				@type[0] = @type[0].capitalize
+				@type = (@type + "Node")
+				@const_type = @type.constantize
+
+				puts @category
+				@category = @withinBrackets.match(/\.(.*)>/).captures.first
+				@category_id = (Category.where("name = ?", @category).first).id
+				puts @category_id
+				
+				@title = line.match(/>(.*)/).captures.first
+				puts @title
 			end
-			@withinBrackets = line.match(/<.*>/).to_s
-			puts @withinBrackets
-			@type = @withinBrackets.match(/<(.*)\./).captures.to_s
-			@category = @withinBrackets.match(/\.(.*)>/).captures.to_s
-			puts @type
-			puts @category
 		end
 	end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131121171432) do
+ActiveRecord::Schema.define(version: 20131123173758) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20131121171432) do
     t.integer  "work_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   add_index "nodes", ["category_id"], name: "index_nodes_on_category_id"
