a23bc5d4da69c0e0d73d0e35f2112301c3228183
liamfd <liamfd@gmail.com>
2014-03-31 15:57:42 -0400

    got the spinner working

--- app/assets/javascripts/categories.js
+++ app/assets/javascripts/categories.js
@@ -4,11 +4,36 @@ $( document ).ready(function() {
 		submitForm();
 	});
 	*/
-	$ ('#category_name').get(0).onblur= submitForm;
-  
+
+	$('#category_name').get(0).onblur= submitForm;
+	//$("#spinner").hide();
+/*	$("#spinner").bind("ajaxSend", function() {
+	$(this).show();
+	}).bind("ajaxStop", function() {
+	$(this).hide();
+	alert("scoop");
+	}).bind("ajaxError", function() {
+	$(this).hide();
+	});*/
+	//$("form[data-remote]").bind('ajax:before', toggleSpinner());
+	//$("form[data-remote]").bind('ajax:complete', toggleSpinner());
+	
 });
 
 function submitForm(){
 	$(this).parents("form").submit();
 	console.log($(this));
 }
+
+function toggleSpinner(){
+	$("#spinner").toggle();
+}
+
+$(function() {
+	$("form").bind("ajax:beforeSend", function(){
+		$("#spinner").show();
+	});
+	$("form").bind("ajax:complete", function(){
+		$("#spinner").hide();
+	});
+});
