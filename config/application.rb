b570ab1152594b91b180e5fa42ae5a6ccf76d072
liamfd <liamfd@gmail.com>
2014-04-04 18:06:12 -0400

    redid the application.scss, allows for not including styles in individual stylesheets

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Noteweb
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # add app/assets/fonts to the asset path
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
